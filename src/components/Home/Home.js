c024122abf55b78d0a830679e8fd45f62d8e5fe5
nhoxanboc <nhoxanbocpractice@gmail.com>
2026-04-18 05:37:30 -0700

    Update Page Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
+import homeLogo from "../../Assets/avatar_new.png";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
@@ -12,41 +12,44 @@ import {
 import { FaLinkedinIn } from "react-icons/fa";
 import {FaXTwitter} from 'react-icons/fa6';
 import { SiGmail } from "react-icons/si";
+import Tilt from "react-parallax-tilt";
+
 
 function Home() {
   return (
     <section>
-      <Container fluid className="home-section" id="home">
-        <Particle />
-        <Container className="home-content">
+      <Container fluid className="home-about-section" id="about">
+        {/* <Particle /> */}
+        <Container className="home-about-description">
           <Row>
-            <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
+            <Col md={8} className="home-header">
+              <h1 style={{ paddingBottom: 15 , textAlign: "center"  }} className="heading">
                 Hi There!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
                   👋🏻
                 </span>
               </h1>
 
-              <h1 className="heading-name">
+              <h1 className="heading-name" style={{ padding: 15, textAlign: "center" }}>
                 I'M
                 <strong className="main-name"> 𝓝𝑮𝓤𝓨𝓔𝓝 𝑳𝓔
 
- </strong>
+                </strong>
               </h1>
 
-              <div style={{ padding: 50, textAlign: "left" }}>
-                <Type />
+              <div style={{ padding: 15, textAlign: "center" }}>
+                <Type  />
               </div>
             </Col>
 
-            <Col md={5} style={{ paddingBottom: 20 }}>
-              <img
-                src={homeLogo}
-                alt="home pic"
-                className="img-fluid"
-                style={{ maxHeight: "450px" }}
-              />
+            <Col md={4} style={{ paddingBottom: 20 }} >
+              <Tilt>
+                <img
+                  src={homeLogo}
+                  alt="home pic"
+                  className="img-fluid" 
+                />
+              </Tilt>
             </Col>
           </Row>
         </Container>
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/home_main_img.png";
 import Tilt from "react-parallax-tilt";
 
 function Home2() {
